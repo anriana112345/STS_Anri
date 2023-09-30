9efa7aff3e4e92d0dbd625faa7896c09a7ca5ef0
anriana112345 <anriana1908@gmail.com>
2023-09-30 13:05:33 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+$username=12345;
+$password=12345;
+if($username  == 12345 && $password == 12345){
+    echo "anda berhasil login";
+
+}else if($username == 112345 && $password == 12345){
+    echo "maaf username anda salah";
+}else{
+    echo "mohon maaf username dan password anda salah";
+}
